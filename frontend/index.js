e25393ec74a8db532256be3c1f45e5fbadd6fd6f
nishota <a2038toh7s1124@gmail.com>
2023-03-05 15:43:21 +0900

    first commit

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+const users = [{name: "aaa", hoge:"dddd"}];
+console.log(users);
+/* Here is the explanation for the code above:
+1. The first line of the code above is a comment. It is not executed by the computer.
+2. The second line of the code above is a variable declaration. The variable name is "users". The variable value is an array of objects. The array has one object. The object has two properties: "name" and "hoge". The value of the "name" property is "aaa". The value of the "hoge" property is "dddd".
+3. The third line of the code above is a console.log() function. The console.log() function is used to print the value of a variable to the console. The console.log() function is used for debugging purposes. The console.log() function is not used in production code.
+*/
+/* 上記のコードの説明:
+1. 上記のコードの2行目はコメントです。コンピューターによって実行されません。
+2. 上記のコードの2行目は変数宣言です。変数名は"users"です。変数の値はオブジェクトの配列です。配列には1つのオブジェクトがあります。オブジェクトには2つのプロパティがあります。"name"と"hoge"です。"name"プロパティの値は"aaa"です。"hoge"プロパティの値は"dddd"です。
+3. 上記のコードの3行目はconsole.log()関数です。console.log()関数は変数の値をコンソールに出力するために使用されます。console.log()関数はデバッグ目的で使用されます。console.log()関数は本番コードでは使用されません。
+*/
+
